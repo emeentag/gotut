b0bc6b3313c141d77e2b64c51cd7fbcc7dcea4b3
Serdar Simsek <smskserdar@gmail.com>
2020-04-07 02:07:01 +0300

    conditions

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-// Checkout realted branches.
+package main
+
+import "fmt"
+
+func main() {
+	conditions(5)
+	conditions(15)
+	conditions(10)
+}
+
+func conditions(x int) {
+	if x == 10 {
+		fmt.Printf("%d is equal to 10\n", x)
+	} else if x > 10 {
+		fmt.Printf("%d is greater than 10\n", x)
+	} else {
+		fmt.Printf("%d is less than 10\n", x)
+	}
+
+}
