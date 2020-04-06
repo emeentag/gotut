342b077616dde4c641612fb23ad1554be3f90184
Serdar Simsek <smskserdar@gmail.com>
2020-04-07 02:11:46 +0300

    loops

--- main.go
+++ main.go
@@ -3,18 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	maps()
+	loops()
 }
 
-func maps() {
-	vertices := make(map[string]int)
-	vertices["one"] = 1
-	vertices["two"] = 2
-	vertices["three"] = 3
-
-	fmt.Println(vertices)
-	fmt.Println(vertices["two"])
-
-	delete(vertices, "two")
-	fmt.Println("deleted", vertices)
+func loops() {
+	for i := 0; i < 5; i++ {
+		fmt.Printf("idx: %d\n", i)
+	}
+	fmt.Println("------------loop like while-------------")
+	j := 0
+	for j < 5 {
+		fmt.Printf("jdx: %d\n", j)
+		j++
+	}
+	fmt.Println("------------loop in arr-------------")
+	arr := []int{1, 2, 3, 4, 5}
+	for idx, val := range arr {
+		fmt.Println("index", idx, "value", val)
+	}
 }
